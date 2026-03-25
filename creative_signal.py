fe1f5a7f276502fe8172cc809262733da2fc5a63
CorentinRaffray <raffray.corentin@gmail.com>
2026-03-25 13:25:56 +0100

    partie 5, question 1 en entier

--- creative_signal.py
+++ creative_signal.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 data_votes = pd.read_parquet('../data/7651fd0b-f222-43b3-8db8-ed6ae660d313')
+data_reactions = pd.read_parquet('../data/4ffc86e1-84a4-4fdc-9726-66408e596fef')
 
 print(data_votes.head(5))
 print(data_votes.columns)
 print(data_votes.shape)
+
+print(data_reactions.head(5))
+print(data_reactions.columns)
+print(data_reactions.shape)
